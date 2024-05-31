ffb045cf76d354c407ad04a2b968bd659f2fbd1f
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-05-31 11:41:06 +0530

    Create main.tf

---
+++ .buildkite/slave/file/main.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = ">= 1.7.0"
+}
+
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = merge(
+      var.additional_tags
+    )
+  }
+}
+
+locals {
+  name = "${var.app_name}-${var.environment}-${var.region_id}-${var.domain}"
+}
