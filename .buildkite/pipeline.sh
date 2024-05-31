b3a3594ebaab355d91cb796e6dc7c60265a6659c
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-05-31 11:48:05 +0530

    Update pipeline.sh

--- .buildkite/pipeline.sh
+++ .buildkite/pipeline.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
 pwd
+CHANGED_FILES=$(git diff origin/main...HEAD --name-only | grep '.buildkite/slave/file/')
+  if [[ -z "$CHANGED_FILES" ]]; then
+    echo "Changes detected in AWS directory."
+    export RUN_AWS_STEP=true
+  else
+    echo "No changes detected in AWS directory."
+    unset RUN_AWS_STEP
+  fi
