a42091fb43b1778f95163c5f6e907c69ee0e237b
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-05-31 12:26:54 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -11,9 +11,11 @@ PREVIOUS_COMMIT="${BUILDKITE_PREVIOUS_COMMIT:-HEAD~1}"
 if git diff --name-only "$PREVIOUS_COMMIT" "$CURRENT_COMMIT" -- "$DIRECTORY_TO_CHECK" | grep -q "$DIRECTORY_TO_CHECK"; then
   echo "Changes detected in $DIRECTORY_TO_CHECK"
   export RUN_AWS_STEP=true
+  buildkite-agent meta-data set RUN_AWS_STEP "true"
   exit 0
 else
   echo "No changes detected in $DIRECTORY_TO_CHECK"
   export RUN_AWS_STEP=false
+  buildkite-agent meta-data set RUN_AWS_STEP "false"
   echo $RUN_AWS_STEP
 fi
