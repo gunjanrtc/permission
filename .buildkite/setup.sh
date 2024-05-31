1a5c218e96c4b2d9fc050513c542b52301f34431
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-05-31 12:18:22 +0530

    Update setup.sh

--- .buildkite/setup.sh
+++ .buildkite/setup.sh
@@ -2,14 +2,13 @@
 
 # Directory to check for changes
 DIRECTORY_TO_CHECK="terraform/aws/"
-# Get the current commit
-CURRENT_COMMIT=$(git rev-parse HEAD)
+CURRENT_COMMIT="${BUILDKITE_COMMIT:-HEAD}"
 
-# Get the previous commit from the main branch
-PREVIOUS_COMMIT=$(git rev-parse HEAD^)
+# Get the previous commit (default to HEAD~1 if not set)
+PREVIOUS_COMMIT="${BUILDKITE_PREVIOUS_COMMIT:-HEAD~1}"
 
 # Check for changes in the specified directory
-if git diff --name-only "$BUILDKITE_COMMIT" "$BUILDKITE_PREVIOUS_COMMIT" -- "$DIRECTORY_TO_CHECK" | grep -q "$DIRECTORY_TO_CHECK"; then
+if git diff --name-only "$PREVIOUS_COMMIT" "$CURRENT_COMMIT" -- "$DIRECTORY_TO_CHECK" | grep -q "$DIRECTORY_TO_CHECK"; then
   echo "Changes detected in $DIRECTORY_TO_CHECK"
   export RUN_AWS_STEP=true
   exit 0
