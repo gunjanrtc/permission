afb6a0a5e1fed882a9f6fb16dda1867054a38ae0
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-05-31 12:06:25 +0530

    Create setup.sh

---
+++ .buildkite/setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Directory to check for changes
+DIRECTORY_TO_CHECK="terraform/aws/"
+
+# Check for changes in the specified directory
+if git diff --name-only "$BUILDKITE_COMMIT" "$BUILDKITE_PREVIOUS_COMMIT" | grep -q "^$DIRECTORY_TO_CHECK"; then
+  echo "Changes detected in $DIRECTORY_TO_CHECK"
+  export RUN_AWS_STEP=true
+  exit 0
+else
+  echo "No changes detected in $DIRECTORY_TO_CHECK"
+  export RUN_AWS_STEP=false
+  exit 1
+fi
