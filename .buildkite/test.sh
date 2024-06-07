d8465ede79259fefb0e110317da8fc40411cab86
gunjanrtc <156164197+gunjanrtc@users.noreply.github.com>
2024-06-07 15:07:19 +0530

    added code

--- .buildkite/test.sh
+++ .buildkite/test.sh
@@ -1,4 +1,37 @@
 #!/bin/bash
 
-file_changed=$(git log -n 1 --oneline | grep terraform/aws/)
-echo $file_changed
+init() {
+    pwd
+    echo "starting build: $BUILDKITE_BUILD_NUMBER"
+    echo "this is init"
+    }
+
+setup() {
+    pwd 
+    mkdir -p /pipeline_code_files
+    cd /pipeline_code_files
+    pwd
+}
+
+run() {
+    echo "Running test cases..."
+    # Add your test case execution steps here
+    echo "Test cases execution completed."
+    cd /pipeline_code_files
+    pwd
+}
+
+clean() {
+    echo "cleaning stage"
+    rm /pipeline_code_files
+}
+
+if [[ "$1" == "init"]]; then
+    init
+elif [[ "$1" == "setup"]]; then
+    setup
+elif [[ "$1" == "run"]]; then
+    run
+elif [[ "$1" == "clean"]]; then
+    clean
+fi
